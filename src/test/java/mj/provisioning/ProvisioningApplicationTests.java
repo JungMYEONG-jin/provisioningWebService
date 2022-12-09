1e6a01ddae36eb5e1cbe65858b6308cf72868e41
a60156077 <dwrhksfl77@naver.com>
2022-12-09 17:30:05 +0900

    ignore 적용

---
+++ src/test/java/mj/provisioning/ProvisioningApplicationTests.java
@@ -0,0 +1,13 @@
+package mj.provisioning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvisioningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
