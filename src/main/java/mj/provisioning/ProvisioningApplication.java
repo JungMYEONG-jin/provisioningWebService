2cad57b637201ca4037a2f694a0fde8d836f4160
a60156077 <dwrhksfl77@naver.com>
2022-12-14 16:30:20 +0900

    ignore 수정

--- src/main/java/mj/provisioning/ProvisioningApplication.java
+++ src/main/java/mj/provisioning/ProvisioningApplication.java
@@ -1,7 +1,13 @@
 package mj.provisioning;
 
+import mj.provisioning.device.application.port.in.DeviceUseCase;
+import mj.provisioning.profile.application.port.in.ProfileUseCase;
+import mj.provisioning.profiledevice.application.port.in.ProfileDeviceUseCase;
+import mj.provisioning.test.TestDataInit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class ProvisioningApplication {
@@ -10,4 +16,16 @@ public class ProvisioningApplication {
 		SpringApplication.run(ProvisioningApplication.class, args);
 	}
 
+	/**
+	 * for test data insert
+	 * @param profileUseCase
+	 * @param profileDeviceUseCase
+	 * @param deviceUseCase
+	 * @return
+	 */
+	@Bean
+	@Profile("aws")
+	public TestDataInit testDataInit(ProfileUseCase profileUseCase, ProfileDeviceUseCase profileDeviceUseCase, DeviceUseCase deviceUseCase){
+		return new TestDataInit(profileUseCase, profileDeviceUseCase, deviceUseCase);
+	}
 }
--- src/main/java/mj/provisioning/test/TestDataInit.java
+++ src/main/java/mj/provisioning/test/TestDataInit.java
@@ -4,9 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mj.provisioning.device.application.port.in.DeviceUseCase;
 import mj.provisioning.profile.application.port.in.ProfileUseCase;
+import mj.provisioning.profile.domain.Profile;
 import mj.provisioning.profiledevice.application.port.in.ProfileDeviceUseCase;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,10 +20,12 @@ public class TestDataInit {
     private final DeviceUseCase deviceUseCase;
 
 //A2BFWL3C73
-//    @EventListener(ApplicationReadyEvent.class)
-//    public void init(){
-//        profileUseCase.saveProfiles(); // 모든 profiles 업데이트
-//        deviceUseCase.saveDevices(); // 모든 device 업데이트
-//    }
+    @EventListener(ApplicationReadyEvent.class)
+    public void init(){
+        profileUseCase.saveProfiles(); // 모든 profiles 업데이트
+        deviceUseCase.saveDevices(); // 모든 device 업데이트
+        List<Profile> all = profileUseCase.findAll();
+        all.forEach(profile -> profileDeviceUseCase.saveProfileDevice(profile.getProfileId()));
+    }
 
 }
