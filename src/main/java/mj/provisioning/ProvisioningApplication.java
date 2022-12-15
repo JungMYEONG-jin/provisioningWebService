71d2b6cf63b80374c4e789a6ed701248ecb22972
a60156077 <dwrhksfl77@naver.com>
2022-12-15 16:50:51 +0900

    bean 으로 등록하면?

--- src/main/java/mj/provisioning/ProvisioningApplication.java
+++ src/main/java/mj/provisioning/ProvisioningApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProvisioningApplication {
@@ -17,6 +19,20 @@ public class ProvisioningApplication {
 		SpringApplication.run(ProvisioningApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer CORSConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedHeaders("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+						.maxAge(-1)   // add maxAge
+						.allowCredentials(false);
+			}
+		};
+	}
 	/**
 	 * for test data insert
 	 * @param profileUseCase
