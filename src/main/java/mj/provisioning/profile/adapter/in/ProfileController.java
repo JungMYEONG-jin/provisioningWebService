32b5254d6bb28099e7bab9b6d1b86de5354dce1d
a60156077 <dwrhksfl77@naver.com>
2022-12-27 15:59:42 +0900

    upload to server

--- src/main/java/mj/provisioning/profile/adapter/in/ProfileController.java
+++ src/main/java/mj/provisioning/profile/adapter/in/ProfileController.java
@@ -1,14 +1,14 @@
 package mj.provisioning.profile.adapter.in;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mj.provisioning.profile.application.port.in.*;
+import mj.provisioning.profile.domain.Profile;
+import mj.provisioning.util.FileUploadUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.tmatesoft.svn.core.SVNException;
 
-import java.net.URI;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -17,6 +17,7 @@ import java.util.List;
 public class ProfileController {
 
     private final ProfileUseCase profileUseCase;
+    private final FileUploadUtils fileUploadUtils;
 
     @GetMapping("/apple/profile/list")
     public ResponseEntity<ProfileShowListDto> getList(ProfileSearchCondition condition){
@@ -39,7 +40,12 @@ public class ProfileController {
 
     @PostMapping(value = "/resources/profiles/edit/{profileId}", produces = "application/text; charset=UTF-8")
     public ResponseEntity postEdit(@PathVariable(name = "profileId") String profileId, @RequestBody ProfileEditRequestDto profileEditRequestDto){
-        profileUseCase.editProvisioning(profileEditRequestDto);
+        Profile profile = profileUseCase.editProvisioning(profileEditRequestDto);
+        try {
+            fileUploadUtils.uploadToSVN("svntest", profile.getName(), profile.getProfileContent());
+        } catch (SVNException e) {
+            throw new RuntimeException("Fail to upload svn");
+        }
         return ResponseEntity.ok("수정에 성공하였습니다.");
     }
 
--- src/main/java/mj/provisioning/util/FileUploadUtils.java
+++ src/main/java/mj/provisioning/util/FileUploadUtils.java
@@ -1,10 +1,16 @@
 package mj.provisioning.util;
 
 import lombok.extern.slf4j.Slf4j;
-import org.tmatesoft.svn.core.SVNCommitInfo;
-import org.tmatesoft.svn.core.SVNException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.tmatesoft.svn.core.*;
+import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
+import org.tmatesoft.svn.core.internal.io.dav.DAVRepositoryFactory;
 import org.tmatesoft.svn.core.io.ISVNEditor;
+import org.tmatesoft.svn.core.io.SVNRepository;
+import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
 import org.tmatesoft.svn.core.io.diff.SVNDeltaGenerator;
+import org.tmatesoft.svn.core.wc.SVNWCUtil;
 
 import javax.xml.bind.DatatypeConverter;
 import java.io.*;
@@ -14,8 +20,15 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 @Slf4j
+@Component
 public class FileUploadUtils {
-    public static void saveProvisioning(String toUploadDir, String fileName, InputStream inputStream){
+
+    @Value("${svn.user}")
+    private static String user;
+    @Value("${svn.password}")
+    private static String pw;
+
+    public void saveProvisioning(String toUploadDir, String fileName, InputStream inputStream){
         Path uploadPath = Paths.get(System.getProperty("user.dir")+toUploadDir);
 
         if (!Files.exists(uploadPath)) {
@@ -35,7 +48,7 @@ public class FileUploadUtils {
         }
     }
 
-    public static void writeProvisioning(String profileContent, String name, String toUploadDir){
+    public void writeProvisioning(String profileContent, String name, String toUploadDir){
         byte[] data = DatatypeConverter.parseBase64Binary(profileContent);
         String fileName = name+".mobileprovision";
         Path uploadPath = Paths.get(System.getProperty("user.dir")+toUploadDir);
@@ -57,7 +70,7 @@ public class FileUploadUtils {
         }
     }
 
-    public static void cleanDir(String dir){
+    public void cleanDir(String dir){
         Path dirPath = Paths.get(System.getProperty("user.dir")+dir);
         try{
             Files.list(dirPath).forEach(file-> {
@@ -74,7 +87,52 @@ public class FileUploadUtils {
         }
     }
 
-    public static SVNCommitInfo addFile(ISVNEditor editor, String filePath, byte[] data) throws SVNException {
+    /**
+     * checkout example
+     * https 연결 초기화를 한다.
+     * SVNRepository을 연다
+     * User 인증 정보는 ISVNAuthenticationManager 통해 제공 된다.
+     * repository에 인증 정보를 세팅한다.
+     * 해당 url에 지정된 node type 파악하고 directory가 아니라면 종료시킨다.
+     * 가장 최신 헤드 버전을 가져온다.
+     * commit 전까지 SVNRepository의 메소드를 invoke 하면 안된다.
+     * commit 메세지는 커밋 로그에 남는다.
+     * ISVNWorkspaceMediator 는 임시 파일을 저장하기 위해 사용된다. 만약 null 이 넘겨지면 기본 임시 폴더가 사용될것이다.
+     * 작업중인 svn url에 폴더나 파일을 add하자.
+     * SVNCommitInfo는 기본 커밋 정보를 담는다.
+     * Revision number, 작성자, 커밋 날짜, 커밋 메세지등
+     * 프로비저닝 디코드
+     * @param savePath
+     * @param name
+     * @param data
+     * @throws SVNException
+     */
+    public void uploadToSVN(String savePath, String name, String data) throws SVNException {
+        DAVRepositoryFactory.setup();
+        SVNURL url = SVNURL.parseURIEncoded("https://10.25.219.102/svn/FILESHARE_REPOSITORY/"+savePath);
+        SVNRepository svnRepository = SVNRepositoryFactory.create(url);
+        ISVNAuthenticationManager authenticationManager = SVNWCUtil.createDefaultAuthenticationManager(user, pw);
+        svnRepository.setAuthenticationManager(authenticationManager);
+
+        SVNNodeKind nodeKind = svnRepository.checkPath("", -1);
+        if (nodeKind == SVNNodeKind.NONE) {
+            SVNErrorMessage err = SVNErrorMessage.create(SVNErrorCode.UNKNOWN, "No entry at URL {0}", url);
+            throw new SVNException(err);
+        } else if (nodeKind == SVNNodeKind.FILE) {
+            SVNErrorMessage err = SVNErrorMessage.create(SVNErrorCode.UNKNOWN, "Entry at URL {0} is a file while directory ws expected", url);
+            throw new SVNException(err);
+        }
+        // get latest head version
+        long latestRevision = svnRepository.getLatestRevision();
+        log.info("latestRevision = " + latestRevision);
+        ISVNEditor editor = svnRepository.getCommitEditor("provisioning test", null);
+        byte[] contents = DatatypeConverter.parseBase64Binary(data);
+        SVNCommitInfo commitInfo = addFile(editor,name+".mobileprovision", contents);
+        log.info("commitInfo = {}", commitInfo);
+    }
+
+
+    private SVNCommitInfo addFile(ISVNEditor editor, String filePath, byte[] data) throws SVNException {
 
         /**
          * 항상 첫번째를 연다. 현재 root dir을 연다. 즉 다른 엔트리가 열리거나 추가전까진 해당 dir에서 작업을 한다는걸 의미한다.
@@ -103,7 +161,7 @@ public class FileUploadUtils {
         return editor.closeEdit();
     }
 
-    public static SVNCommitInfo addDir(ISVNEditor editor, String dir, String filePath, byte[] data) throws SVNException {
+    private SVNCommitInfo addDir(ISVNEditor editor, String dir, String filePath, byte[] data) throws SVNException {
 
         /**
          * 항상 첫번째를 연다. 현재 root dir을 연다. 즉 다른 엔트리가 열리거나 추가전까진 해당 dir에서 작업을 한다는걸 의미한다.
