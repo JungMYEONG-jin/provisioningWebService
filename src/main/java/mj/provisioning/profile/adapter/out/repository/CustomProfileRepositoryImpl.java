69dbce0a8606528d87ffb5e749ee9f0133bd1285
godcoder <dwrhksfl77@naver.com>
2023-01-05 00:16:27 +0900

    검색 조건 수정.

--- src/main/java/mj/provisioning/profile/adapter/out/repository/CustomProfileRepositoryImpl.java
+++ src/main/java/mj/provisioning/profile/adapter/out/repository/CustomProfileRepositoryImpl.java
@@ -10,10 +10,12 @@ import mj.provisioning.profile.domain.Profile;
 import mj.provisioning.profile.domain.ProfilePlatform;
 import mj.provisioning.profile.domain.ProfileType;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
 import static mj.provisioning.profile.domain.QProfile.profile;
+import static org.springframework.util.StringUtils.*;
 import static org.springframework.util.StringUtils.hasText;
 
 @Repository
@@ -36,10 +38,10 @@ public class CustomProfileRepositoryImpl implements CustomProfileRepository{
 
     private BooleanExpression profileTypeEq(ProfileType profileType){
         if (profileType!=null){
-            if (profileType.equals(ProfileType.IOS_APP_DEVELOPMENT)) {
-                return profile.profileType.eq(profileType);
-            } else if(profileType.equals(ProfileType.IOS_APP_STORE) || profileType.equals(ProfileType.IOS_APP_ADHOC)) {
+            if (profileType.equals(ProfileType.IOS_APP_STORE) || profileType.equals(ProfileType.IOS_APP_ADHOC)) {
                 return profile.profileType.eq(ProfileType.IOS_APP_ADHOC).or(profile.profileType.eq(ProfileType.IOS_APP_STORE));
+            } else {
+                return profile.profileType.eq(profileType);
             }
         }
         return null;
@@ -52,6 +54,18 @@ public class CustomProfileRepositoryImpl implements CustomProfileRepository{
      */
     @Override
     public List<ProfileShowDto> getSearchByCondition(ProfileSearchCondition condition) {
+        // 타입 검색 조건 확인
+        BooleanExpression typeEq = profileTypeEq(ProfileType.get(condition.getProfileType()));
+        // 플랫폼 검색 조건 확인
+        BooleanExpression platformEq = profilePlatformEq(ProfilePlatform.get(condition.getProfilePlatform()));
+
+        // 문자열 값이 있는데 ProfileType 에서 해당 조건의 value를 못찾으면 잘못된 검색임. then return isNull
+        if (hasText(condition.getProfileType()) && ProfileType.get(condition.getProfileType()) == null) {
+            typeEq = profile.profileType.isNull();
+        }
+        if (hasText(condition.getProfilePlatform()) && ProfilePlatform.get(condition.getProfilePlatform()) == null) {
+            platformEq = profile.platform.isNull();
+        }
 
         return jpaQueryFactory.select(Projections.fields(ProfileShowDto.class,
                         profile.id.as("key"),
@@ -62,8 +76,8 @@ public class CustomProfileRepositoryImpl implements CustomProfileRepository{
                         profile.expirationDate))
                 .from(profile)
                 .where(nameLike(condition.getName()),
-                        profilePlatformEq(ProfilePlatform.get(condition.getProfilePlatform())),
-                        profileTypeEq(ProfileType.get(condition.getProfileType())))
+                        platformEq,
+                        typeEq)
                 .fetch();
     }
 
